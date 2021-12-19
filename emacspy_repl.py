4db9bd003bbbc0b54a3c881aefb6663407849073
Shawn Presser <shawnpresser@gmail.com>
2021-12-18 20:54:41 -0600

    Move the runtime into empy

--- emacspy_repl.py
+++ emacspy_repl.py
@@ -1,20 +1,20 @@
-import emacspy as em
-from emacspy import V, F, Q, S, L, E
-
-import code
-import readline
-import rlcompleter
-
-def dbg():
-  breakpoint()
-
-def configure(ns=globals()):
-  readline.set_completer(rlcompleter.Completer(ns).complete)
-  readline.parse_and_bind("tab: complete")
-
-def interact(ns=globals()):
-  configure(ns)
-  code.interact("*interactive*", local=ns)
-
-configure()
-interact()
+# import emacspy as em
+# from emacspy import V, F, Q, S, L, E
+# 
+# import code
+# import readline
+# import rlcompleter
+# 
+# def el_configure(ns=globals()):
+#   readline.set_completer(rlcompleter.Completer(ns).complete)
+#   readline.parse_and_bind("tab: complete")
+# 
+# def interact(ns=globals()):
+#   configure(ns)
+#   code.interact("*interactive*", local=ns)
+# 
+# configure()
+# interact()
+# import site
+# site.enablerlcompleter()
+el_repl()
